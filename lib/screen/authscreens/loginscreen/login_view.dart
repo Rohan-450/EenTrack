4ad533d35452c36331fcd1d283e05be998aa67d8
NabajitBhadury <nabajitbhadury@gmail.com>
2023-08-16 08:38:10 +0530

    Test commit

--- lib/screen/authscreens/loginscreen/login_view.dart
+++ lib/screen/authscreens/loginscreen/login_view.dart
@@ -19,30 +19,68 @@ class _LoginViewState extends State<LoginView> {
       child: Form(
         key: _formKey,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView(
-            children: [
-              const SizedBox(height: 100),
-              Image.asset('assets/logo.png', height: 100),
-              const SizedBox(height: 50),
-              CustomTextBox(
-                  label: "Email",
-                  onChanged: (email) {
-                    this.email = email;
-                  }),
-              const SizedBox(height: 20),
-              CustomTextBox(
-                  label: "Password",
-                  onChanged: (password) {
-                    this.password = password;
-                  }),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Login"),
+          padding: const EdgeInsets.all(6.0),
+          child: SingleChildScrollView(
+              child: Column(children: [
+            Column(
+              children: [
+                Image.asset('assets/logo.png', height: 100),
+                const SizedBox(
+                  height: 50,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 100,
+              child: Text(
+                "Welcome Back!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              width: 350,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  color: const Color.fromARGB(255, 59, 55, 55),
+                  child: CustomTextBox(
+                      label: "Email",
+                      onChanged: (email) {
+                        this.email = email;
+                      }),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 350,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  color: const Color.fromARGB(255, 59, 55, 55),
+                  child: CustomTextBox(
+                      label: "Password",
+                      onChanged: (password) {
+                        this.password = password;
+                      }),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all(
+                    const Size(200, 50),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(Colors.cyan)),
+              child: const Text(
+                "Login",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ])),
         ),
       ),
     );
--- lib/screen/authscreens/shared/customtextbox.dart
+++ lib/screen/authscreens/shared/customtextbox.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class CustomTextBox extends StatelessWidget {
   final String label;
-  final  Function(String) onChanged;
+  final Function(String) onChanged;
   final String? initialValue;
   final bool obscureText;
   final String? Function(String?)? validator;
@@ -19,17 +19,17 @@ class CustomTextBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-			initialValue: initialValue,
-			obscureText: obscureText,
-			onChanged: onChanged,
-			validator: validator,
-			decoration: InputDecoration(
-				labelText: label,
-				border: const OutlineInputBorder(),
-			),
-			onTapOutside: (_){
-			FocusScope.of(context).unfocus();
-			},
-		);
+      initialValue: initialValue,
+      obscureText: obscureText,
+      onChanged: onChanged,
+      validator: validator,
+      decoration: InputDecoration(
+        labelText: label,
+        border: const OutlineInputBorder(),
+      ),
+      onTapOutside: (_) {
+        FocusScope.of(context).unfocus();
+      },
+    );
   }
 }
