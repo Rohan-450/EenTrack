9b0ab3f77d194cc609e96147a895925f5efc3c25
Kaushik Chowdhury <95243692+darkard2003@users.noreply.github.com>
2023-08-22 00:26:51 +0530

    Added loading screen may change in future

---
+++ lib/screen/shared/loading_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
